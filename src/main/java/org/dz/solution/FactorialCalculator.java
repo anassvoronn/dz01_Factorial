4089f55c4c95836aad90a3a1cfbead40ba392780
alexander.budnikov <alexander.budnikov@algosec.com>
2022-07-09 14:43:13 +0300

    Fixed error message

--- src/main/java/org/dz/solution/FactorialCalculator.java
+++ src/main/java/org/dz/solution/FactorialCalculator.java
@@ -3,7 +3,7 @@ package org.dz.solution;
 public class FactorialCalculator {
     public int calculate(int number) {
         if (number < 0) {
-            throw new IllegalArgumentException("Number can not be negative");
+            throw new IllegalArgumentException("Number can't be negative");
         }
         int result = 1;
         for (int i = 1; i <= number; i++) {
